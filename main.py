758b89f3c95cc0818f228e59f9d647f3b9dd03f4
v744410-dev <v744410@gmail.com>
2026-05-22 18:45:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-# Хранилище фильмов (в памяти)
+# Хранилище фильмов
 movies = []
 next_id = 1
 
 @app.route('/')
-def root():
-    return {"message": "Видеотека API работает!", "endpoints": ["GET /movies", "POST /movies", "DELETE /movies/<id>"]}
+def home():
+    return {"message": "Видеотека API работает!"}
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
@@ -20,8 +20,7 @@ def get_movies():
 @app.route('/movies', methods=['POST'])
 def add_movie():
     global next_id
-    data = request.json
-    
+    data = request.get_json()
     movie = {
         "id": next_id,
         "title": data.get('title'),
@@ -32,13 +31,13 @@ def add_movie():
     }
     movies.append(movie)
     next_id += 1
-    return jsonify(movie), 201
+    return jsonify(movie)
 
 @app.route('/movies/<int:movie_id>', methods=['DELETE'])
 def delete_movie(movie_id):
     global movies
     movies = [m for m in movies if m['id'] != movie_id]
-    return jsonify({"message": "Фильм удален"})
+    return jsonify({"message": "deleted"})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
